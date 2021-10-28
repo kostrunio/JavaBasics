07f8e4ec04ea0734c6d73a7472b0f9f88a6d7ad2
Michał Kostro <michal.kostro@10g.pl>
2021-10-28 23:21:06 +0200

    array - end

--- src/ArraysTestClass.java
+++ src/ArraysTestClass.java
@@ -1,5 +1,21 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class ArraysTestClass {
     public static void main(String[] args) {
+        int[] array = new int[5]; //array with length of 0 - empty array
+        String[] words = new String[5]; //array with length of 5
+        double[] numbers = {1.2, 2.3, 3.4, 4.5}; //array with initialization - 4 elements
 
+        words[0] = "first"; //index of elements starts from 0!!
+        System.out.println(words[0]); //print first element from an array
+        System.out.println(words); //wrong way to print array
+        System.out.println(Arrays.toString(numbers));
+        words[4] = "last"; //exception!! last element has index 4 (because we start from 0)
+
+        // change array to size 5 and print it
+        System.out.println(Arrays.toString(array));
+        // print words in proper way
+        System.out.println(Arrays.toString(words));
     }
 }
