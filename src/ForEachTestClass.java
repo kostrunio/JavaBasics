b89536683a1a9d1a39984d4c5a124e4d83746eaf
Michał Kostro <michal.kostro@10g.pl>
2021-11-02 23:49:23 +0100

    forEach - end

--- src/ForEachTestClass.java
+++ src/ForEachTestClass.java
@@ -1,5 +1,14 @@
 public class ForEachTestClass {
     public static void main(String[] args) {
+        int[] numbers = new int[10];
+        // from 1 to 10
+        for (int i=0; i < numbers.length; i++) {
+            numbers[i] = i; //numbers[0] = 0; numbers[1] = 1; etc.
+        }
 
+        //forEach
+        for (int number : numbers) {
+            System.out.println(number);
+        }
     }
 }
