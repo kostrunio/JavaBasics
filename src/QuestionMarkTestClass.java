d604d1d589897f10df74b9393dc085f277089b29
Michał Kostro <michal.kostro@10g.pl>
2021-11-04 23:15:02 +0100

    ? - start

---
+++ src/QuestionMarkTestClass.java
@@ -0,0 +1,10 @@
+public class QuestionMarkTestClass {
+    public static void main(String[] args) {
+        int number = 10;
+        if (number > 5) {
+            System.out.println("higer");
+        } else {
+            System.out.println("lower");
+        }
+    }
+}
