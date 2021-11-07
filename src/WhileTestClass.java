7e44694297f4c1302db525c2fec6dd7eaebbc27e
Michał Kostro <michal.kostro@10g.pl>
2021-11-07 23:40:43 +0100

    while - end

--- src/WhileTestClass.java
+++ src/WhileTestClass.java
@@ -1,5 +1,15 @@
+import java.util.Random;
+
 public class WhileTestClass {
     public static void main(String[] args) {
+        Random random = new Random();
+        int result = random.nextInt(10);
 
+        while(result != 0) {
+            System.out.println("result = " + result);
+            result = random.nextInt(10);
+        }
+
+        System.out.println("result is " + result);
     }
 }
