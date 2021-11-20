468f2809f5a03826721db73ae3ed2329dd3004ae
Michał Kostro <michal.kostro@10g.pl>
2021-11-20 23:52:11 +0100

    scanner - end

--- src/org/examples/hackerrank/ScannerTestClass.java
+++ src/org/examples/hackerrank/ScannerTestClass.java
@@ -1,7 +1,15 @@
 package org.examples.hackerrank;
 
+import java.util.Scanner;
+
 public class ScannerTestClass {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write something and press Enter");
+        double input = scanner.nextDouble();
+        scanner.nextLine();
+        String nextInput = scanner.nextLine();
+        System.out.println("input: " + input);
+        System.out.println("nextInput: " + nextInput);
     }
 }
