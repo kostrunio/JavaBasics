934fd26596f65a17336b2df157075b76a6f49aee
Michał Kostro <michal.kostro@10g.pl>
2021-11-22 23:14:36 +0100

    math - end

--- src/org/examples/hackerrank/MathTestClass.java
+++ src/org/examples/hackerrank/MathTestClass.java
@@ -2,6 +2,16 @@ package org.examples.hackerrank;
 
 public class MathTestClass {
     public static void main(String[] args) {
+        double lower = 3.1;
+        double higher = 3.9;
 
+        System.out.println(Math.round(lower));//3.1 -> 3
+        System.out.println(Math.round(higher));//3.9 -> 4
+
+        System.out.println(Math.PI);
+        System.out.println(Math.abs(-3.5));
+
+        System.out.println(Math.pow(2,3));
+        System.out.println(Math.random()*100);//0-100
     }
 }
