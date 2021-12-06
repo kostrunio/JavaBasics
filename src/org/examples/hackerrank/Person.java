72ea44e6fb2e93a398a5e563aec1433814a245bf
Michał Kostro <michal.kostro@10g.pl>
2021-12-06 23:09:40 +0100

    class - start

---
+++ src/org/examples/hackerrank/Person.java
@@ -0,0 +1,5 @@
+package org.examples.hackerrank;
+
+public class Person {
+
+}
---
+++ src/org/examples/hackerrank/PersonMain.java
@@ -0,0 +1,7 @@
+package org.examples.hackerrank;
+
+public class PersonMain {
+    public static void main(String[] args) {
+
+    }
+}
