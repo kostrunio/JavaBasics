4387426444f3d71e54f3f3cd01cc0771c2de1f5c
Michał Kostro <michal.kostro@10g.pl>
2021-12-06 23:41:10 +0100

    class - end

--- src/org/examples/hackerrank/Person.java
+++ src/org/examples/hackerrank/Person.java
@@ -1,5 +1,19 @@
 package org.examples.hackerrank;
 
 public class Person {
+    private String name;
+    private int age;
 
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void whoAmI() {
+        System.out.println("my name is " + name + ", and I'm " + age + " years old");
+    }
 }
--- src/org/examples/hackerrank/PersonMain.java
+++ src/org/examples/hackerrank/PersonMain.java
@@ -2,6 +2,9 @@ package org.examples.hackerrank;
 
 public class PersonMain {
     public static void main(String[] args) {
+        Person person = new Person("michał", 38);
+        System.out.println(person.getName());
 
+        person.whoAmI();
     }
 }
