459fc122b761ecc1d1d7219101d276743097f554
Michał Kostro <michal.kostro@10g.pl>
2021-12-11 22:27:08 +0100

    string - end

--- src/org/examples/hackerrank/StringTestClass.java
+++ src/org/examples/hackerrank/StringTestClass.java
@@ -2,6 +2,10 @@ package org.examples.hackerrank;
 
 public class StringTestClass {
     public static void main(String[] args) {
-
+        String newLine = "our new line";//char ''
+        for (int i=0; i<newLine.length(); i++) {
+            char element = newLine.charAt(i);
+            System.out.println(element);//o
+        }
     }
 }
