412172d9c4337d2c18f1f2f8cdcd2be71f2ead94
Michał Kostro <michal.kostro@10g.pl>
2022-01-27 22:19:54 +0100

    map - end

--- src/org/examples/hackerrank/MapTestClass.java
+++ src/org/examples/hackerrank/MapTestClass.java
@@ -1,4 +1,14 @@
 package org.examples.hackerrank;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapTestClass {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("michal", 123456789);
+
+        System.out.println("michal's phone number:" + map.get("michal"));
+        System.out.println("karol's phone number:" + map.get("karol"));
+    }
 }
